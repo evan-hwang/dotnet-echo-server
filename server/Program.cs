2a2728506b57c11090150a21e2bea43184a61ca9
Evan Hwang <evan.h@kakaocorp.com>
2019-03-13 11:27:42 +0900

    echo client, echo server 프로젝트 생성

---
+++ server/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace echo_server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetworkV6, SocketType.Stream, ProtocolType.Tcp);
+            Console.WriteLine(socket);
+            socket.Close();
+        }
+    }
+}
